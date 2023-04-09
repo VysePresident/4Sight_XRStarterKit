dcd08901734e131c397debc2b3aa12e4d8703619
BuildTools <AgentIllusion73@gmail.com>
2023-04-09 10:20:07 -0500

    Tap To Place with Tap To Make Appear

---
+++ XRStarterKit/Assets/Scripts/TapToMakeAppear.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapToMakeAppear : MonoBehaviour
+{
+    bool cubeCreated = false;
+    public GameObject cubePrefab;
+
+    private GameObject cube = new GameObject();
+    
+    // Give basic edit modes
+    public enum editMode
+    {
+        Add,
+        Move,
+        Rotate
+    }
+
+    public editMode mode;
+
+    void spawnObject()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mode == editMode.Add)
+        {
+            if (Input.GetMouseButtonDown(0) && !cubeCreated)
+            {
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.z = 3; // set the distance from the camera
+                Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+                GameObject newObject = Instantiate(cubePrefab, objectPos, Quaternion.identity);
+                cubeCreated = true;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                cubeCreated = false;
+            }
+        }
+        if (mode == editMode.Move)
+        {
+            Debug.Log("MOVE MODE");
+        }
+        if (mode == editMode.Rotate)
+        {
+            Debug.Log("ROTATE MODE");
+        }
+    }
+}
