8e49306cd83cf553cb534355daee061ce67ba3b6
tamialin <tamialin@gmail.com>
2023-04-09 02:11:33 -0500

    add furniture in AR button

--- XRStarterKit/Assets/Scripts/AddFurniture.cs
+++ XRStarterKit/Assets/Scripts/AddFurniture.cs
@@ -9,8 +9,18 @@ public class AddFurniture : MonoBehaviour
 
     public void OnButtonClick()
     {
-        SceneManager.LoadScene("ARBasic");
-        for(int i = 0; i < 3; i++)
-            Instantiate(furnitureToAdd);
+        SceneManager.LoadScene("ARBasic"); // load Scene A
+
+        SceneManager.sceneLoaded += OnSceneLoaded; // subscribe to the sceneLoaded event
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject groundPlane = GameObject.Find("Ground Plane Stage"); // find the "Ground Plane Stage" object
+        if (groundPlane != null)
+        {
+            GameObject childObject = Instantiate(furnitureToAdd, groundPlane.transform); // instantiate the prefab as a child of the "Ground Plane Stage"
+        }
+
+        SceneManager.sceneLoaded -= OnSceneLoaded; // unsubscribe from the sceneLoaded event
     }
 }
