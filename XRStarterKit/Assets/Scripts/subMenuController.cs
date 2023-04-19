61c9e91aba84442a2070f5cdbb15ab5d72a93aa4
Liang Yu Lin <tanyalin2001@gmail.com>
2023-04-19 02:31:06 -0500

    Basic setup of submenu

---
+++ XRStarterKit/Assets/Scripts/subMenuController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class subMenuController : MonoBehaviour
+{
+	public void Remove()
+	{
+		// Implement the logic to remove furniture
+		Debug.Log("Remove Furniture");
+	}
+
+	public void ShowDetails()
+	{
+		// Implement the logic to show furniture details
+		Debug.Log("Show furniture details");
+	}
+
+	public void AddToCart()
+	{
+		// Implement the logic to add the furniture to the cart
+		Debug.Log("Add furniture to cart");
+	}
+
+	public void Restart()
+	{
+		// Implement the logic to restart or reset the AR view
+		Debug.Log("Restart AR view");
+	}
+
+	public void CloseARView()
+	{
+		// Implement the logic to close the AR view
+		Debug.Log("Close AR view");
+	}
+}
